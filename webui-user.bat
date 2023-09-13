52a721aabf8219d42c542dc26ae8df0ea9160ac4
SenzoShujin <130248797+SenzoShujin@users.noreply.github.com>
2023-09-12 21:22:11 -0400

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON= "C:\Users\epere\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers
 
 call webui.bat
